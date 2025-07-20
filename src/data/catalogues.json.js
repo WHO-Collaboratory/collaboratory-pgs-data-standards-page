6850d269d8943bf8c899bf76a58143cd33c97543
stephaneghozzi <hello@stephaneghozzi.com>
2025-07-20 11:19:48 +0200

    initial commit

---
+++ src/data/catalogues.json.js
@@ -0,0 +1,44 @@
+
+async function json_ok(url) {
+  const response = await fetch(url);
+    return await response.ok;
+}
+
+async function json(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
+    return await response.json();
+}
+
+const repo = "collaboratory-pgs-data-standards";
+const owner = "WHO-Collaboratory";
+const cat_file = "catalogue.json";
+
+const branches_gh = await json(
+  `https://api.github.com/repos/${owner}/${repo}/branches`
+);
+const branches = branches_gh.map(d => d.name);
+const branches_ok = [];
+for (let i = 0; i < branches.length; i++) {
+  branches_ok.push(
+    await json_ok(
+      `https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/${branches[i]}/${cat_file}`
+    )
+  )
+}
+
+const catalogue = [];
+let j = 0;
+for (let i = 0; i < branches.length; i++) {
+  if (branches_ok[i]) {
+    catalogue[j] = {
+      branch: branches[i],
+      catalogue: await json(
+        `https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/${branches[i]}/${cat_file}`
+      )
+    };
+    j++;
+  }
+}
+
+process.stdout.write(JSON.stringify(catalogue));
